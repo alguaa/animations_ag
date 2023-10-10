6e1df9d28680034ce07daa1450132145b84d5f0a
Alexander Gustafsson <alex@Alexanders-MacBook-Air.local>
2023-10-10 10:05:30 +0200

    Initial commit

---
+++ fourier.py
@@ -0,0 +1,56 @@
+import numpy as N
+import matplotlib.pyplot as plt
+
+timesteps = 61
+dt = 1/timesteps
+factor = 3;  #number of periods
+tsRange = timesteps*factor
+
+nn = 1000    #no of points defining a function when made here
+mm = 7      #number of basis functions in the F-expansion
+lw = 1       #line width of plots
+
+#Define function in complex plane
+function2=[]
+for ii in range(nn):
+    if ii<int(nn/4)+1:
+        function2.append(1+1j*4*ii/nn)
+    if ii>int(nn/4) and ii<int(2*nn/4)+1:
+        function2.append(1+4*(ii-int(nn/4))/nn+1j)
+    if ii >int(2*nn/4) and ii<int(3*nn/4)+1:
+        function2.append(2+1j-1j*4*(ii-int(2*nn/4))/nn)
+    if ii>int(3*nn/4):
+        function2.append(2-4*(ii-int(3*nn/4))/nn)
+function2 = N.array(function2)+.1*1j
+
+#Returns positions of positions for all rods for all chosen times
+def myfourier(function,nn,mm):
+    cf = [1./nn*N.sum([function[ii]*N.exp(-2*N.pi*1j*ii*jj/nn)\
+                       for ii in range(0,nn)]) for jj in range(-mm,mm+1)]
+    fourier = [N.sum([cf[ii]*N.exp(2*N.pi*1j*(ii-mm)*tt*dt)\
+                      for ii in range(0,2*mm+1)]) for tt in range(0,timesteps+1)]
+    #sort frequencies to fit coefficients
+    mmAsUsed = [ii for ii in range(-mm,mm+1)];
+    mmSorted = sorted(mmAsUsed,key=abs)
+    idx = [(N.abs(mmSorted[ii]-N.array(mmAsUsed))).argmin() for ii in range(2*mm+1)]
+    apprY = [];Ytvec = [];barvec = []
+    for ttmp in range(tsRange):
+        bars = []
+        bars.append(cf[idx[0]])
+        for ii in range(1,2*mm+1):
+            bars.append(bars[ii-1]+cf[idx[ii]]*N.exp(2*N.pi*1j*(idx[ii]-mm)*ttmp*dt))
+        barvec.append(bars)
+        apprY.append(N.imag(bars[ii]))
+        Ytvec.append(N.real(bars[ii]))    
+    return barvec,apprY,Ytvec
+fig2 = myfourier(N.array(function2),nn,mm)
+
+for ttmp in range(tsRange):
+    print(ttmp)
+    plt.plot(N.array(fig2[2][:ttmp+1]),N.array(fig2[1][:ttmp+1]),color=[0,1,0],linewidth=lw)
+    for ii in range(1,2*mm+1):
+        plt.plot(N.real(fig2[0][ttmp][ii-1:ii+1]),N.imag(fig2[0][ttmp][ii-1:ii+1]),color='magenta',linewidth=lw)
+    plt.xlim([.7,2.3])
+    plt.ylim([-.1,1.3])
+    plt.savefig('mwe/fig'+f"{ttmp:05d}.png")
+    plt.close()
---
+++ wavepacket_1D.py
@@ -0,0 +1,64 @@
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import numpy as N
+import scipy.linalg as SLA
+import matplotlib as mpl
+import os, gc
+
+mpl.rcParams['figure.figsize'] = 16/9*6,6
+
+ymin = -2
+xmin = ymin*16/9
+xmax = abs(xmin);ymax = abs(ymin)
+
+path   = 'DestDir'    #Frame directory
+
+NN = 1000       #number of x coordinates
+pwp = 40        #momentum
+sig = .15       #width of wave packet
+Vmax= pwp**2/2*1. #height of tunneling barrier
+frames = 200    #number of frames
+
+dx = (2*xmax-2*xmin)/NN
+xvec = N.arange(xmin*2,xmax*2,dx)
+b  = -1/2/dx**2
+
+#Finite difference Laplacian 
+diag   = -N.diagflat(2*b*N.ones(NN))
+offdiag = N.diagflat(b*N.ones(NN-1),1)
+D2 = diag+offdiag+offdiag.transpose()
+pot = N.zeros(NN)
+for ii in range(NN):
+    if (ii>int(.495*NN) and ii<int(.505*NN)):
+        pot[ii] = Vmax
+H = D2+N.diagflat(pot)
+
+ev,evec = SLA.eigh(H)
+idx = ev.argsort()
+ev,evec = ev[idx],evec[idx]
+
+initwf = N.exp(-(xvec+2)**2/sig-1j*pwp*(xvec+2)) #initial Gaussian
+init = ev[0]
+cm = N.array([N.sum(initwf*evec[:,mm]) for mm in range(len(ev))])
+cm = cm/SLA.norm(cm)
+psi = [cm[jj]*evec[:,jj] for jj in range(len(ev))]
+
+fig = plt.figure()
+for tt in range(frames):
+    tt2 = tt/frames
+    Psi = psi[0]*N.exp(1j*ev[0]*tt2)
+    for jj in range(1,len(psi)):
+        Psi+=psi[jj]*N.exp(1j*ev[jj]*tt2)
+    plt.xlim([xmin,xmax])
+    plt.ylim([ymin*.3,ymax*.3])
+    plt.gca().add_patch(Rectangle((-4,-10),10,10,linewidth=1,edgecolor=None,facecolor='gray'))
+    if max(pot)>0:
+        plt.fill(xvec,pot/abs(b)*.5,linewidth=1,color='gray')
+    else:
+        plt.fill(xvec,pot/abs(b)*.5,linewidth=1,color='black')
+        
+    plt.plot(xvec,5*N.abs(Psi)**2,linewidth=1,color='red')
+    plt.savefig(path+'/fig'+f"{tt:04d}.png",
+                bbox_inches='tight',pad_inches=0)
+    plt.clf()
+    plt.close("fig")
